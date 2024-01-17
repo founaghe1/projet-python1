4658a6637def0a68ff1b0a01c594e51bc5bdf460
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2024-01-17 15:47:40 +0000

    first step 1

---
+++ exo_calculatrice.py
@@ -0,0 +1,7 @@
+nbre_1 = int(input("Entrez la première valeur: "))
+nbre_2 = int(input("Entrez la deuxième valeur: "))
+
+print(f"La somme de {nbre_1} et {nbre_2} est égale à => {nbre_1 + nbre_2}")
+print(f"La multiplication de {nbre_1} et {nbre_2} est égale à => {nbre_1 * nbre_2}")
+print(f"La soustration de {nbre_1} et {nbre_2} est égale à => {nbre_1 - nbre_2}")
+print(f"La division de {nbre_1} et {nbre_2} est égale à => {nbre_1 / nbre_2}")
--- programme1.py
+++ programme1.py
@@ -1,3 +1,5 @@
+''' 
+
 #Les variables
 salutation = "Bonjour, bienvenue sur mon site web"
 print(salutation)
@@ -63,10 +65,10 @@ print()
 print()
 print("_____Les inputs_____")
 #Afficher un message de bienvenue en fonction du pays d
-country = input("Entrez votre pays: ")
-if country == "France":
+country = input("Entrez votre pays: ").lower()
+if country == "france":
     print("Welcome to France!")
-elif country == "USA":
+elif country == "usa":
     print("Welcome to the USA!")
 else:
     print("Bye")
@@ -77,6 +79,197 @@ if len(name) > 0:
     print("Welcome", name + ", nice to meet you!")
 else:
    print("Entrez un nom")
+   
+'''
 
+print()
+# 2h 29 mn  
+  
+# Manupiler les chaines de caracteres
+    # Changer la casse (A-a ou B-b)
+print("Manupiler les chaines de caracteres")
+print("Changer la casse (A-a ou B-b)") 
+#upper()
+Upper = input("Tappez un mot pour UPPER : ").upper()
+print("Votre mot est en majuscule maintenant : ", Upper)
+
+#lower()
+Lower = input("Tappez un mot pour LOWER : ").lower()
+print("Votre mot est en minuscule maintenant : ", Lower)
+  
+#capitalize()
+Capitalize = input("Tappez un mot pour CAPITALIZE : ").capitalize()
+print("Votre mot est en Capitalize maintenant : ", Capitalize)
+
+#title()
+Title = input("Tappez un texte pour TITLE : ").title()
+print("Votre mot est en TITLE maintenant : ", Title)
+
+print()
+    # Remplacer ou modifier des elements
+#replace()
+print("Remplacer ou modifier des elements")
+Replace = "Bonjour".replace("jour", "soir")
+print("Avec REPLACE Bonjour devient => ", Replace)
+
+ReplaceMultiple = "Bonjour Bonjour".replace(" ", "").replace("jour", "soir")
+print("Avec REPLACE enlevons l'espace et remplaçons => ", ReplaceMultiple)
+
+#strip()
+Strip = " Salut ".strip() #enleve les espace
+print("En utilisant STRIP sans specifier des caracteres, le résultat est : ", Strip)
+
+Strip_2 = " Bonjour ".strip(" ujor") #enleve l'espace du debut et les caracteres
+print("strip_2 enleve l'espace du debut et les caracteres :", Strip_2)
+
+#rstrip()
+Rstrip = " Bonjour ".rstrip(" ujor") #rstrip agit vers la droite
+print("rstrip enleve l'espace et les caracteres vers la droite :", Rstrip)
+
+#lstrip()
+Lstrip = " Bonjour ".lstrip(" ujor") #lstrip agit vers la gauche
+print("lstrip enleve l'espace et les caracteres vers la gauche :", Lstrip)
+
+print()
+    #Separer et joindre
+print("Separer et joindre")
+#split()
+Split = "1, 2, 3, 4, 5".split(", ")
+print("Split de 1, 2, 3, 4, 5 => ", Split)
+
+#joindre
+Join = "-".join("1, 2, 3, 4, 5".split(", "))
+print("Join de '-'.join('1, 2, 3, 4, 5'.split(", ")) => ", Join)
+
+print()
+    #Remplir de zéros
+print("Zfill ou Remplir de zéros")
+
+Zfill = "5".zfill(4)
+print("zfill(4) de 5 => ", Zfill)
+Zfill_2 = "5".zfill(3)
+print("zfill(3) de 5 => ", Zfill_2)
+
+for i in range(1, 1000+1):
+    print(str(i).zfill(3))
+
+
+print()
+    # Les methodes " is "
+print("Les methodes is")
+
+#islower
+is_lower = "bonjour".islower()
+print("islower de bonjour => ", is_lower)
+
+#isupper
+is_upper = "BonjOuR".isupper()
+print("isupper de BonjOuR => ", is_upper)
+is_upper_1 = "BONJOUR".isupper()
+print("isupper de BONJOUR => ", is_upper_1)
+
+#isdigit()
+is_digit = "50".isdigit()
+print("isdigit de '50' =>", is_digit)
+is_digit_1 = "20a".isdigit()
+print("isdigit de '20a' => ", is_digit_1)
+
+print()
+    #Compter le nombre d'occurrences dans chaine de caracteres
+#count()
+print("Compter le nombre d'occurrences dans chaine de caracteres")
+NbOccurence = "Bonjour Bonjour".count("Bonjour")
+print("Le nombre d'occurence de BONJOUR dans NbOccurence est : ", NbOccurence)
+NbOccurence_1 = "Bonjour jour".count(" jour")
+print("Le nombre d'occurence de JOUR est : ", NbOccurence_1)
+
+print()
+    #Rechercher 'l'index' d'un mot ou une suite de caracteres
+print("Rechercher 'l'index' d'un mot ou une suite de caracteres")
+find = "Bonjour le jour".find("jour")
+print("trouve jour => ", find)
+index = "Bonjour le jour".index("le")
+print("trouve le avec index => ", index)
     
     
+    
+Chaine = "Ceci est une petite phrase pour tester la recherche"
+MotATrouver = "petite"
+if MotATrouver.lower() in Chaine.lower():
+    print("Le mot '%s' est present dans la chaine." % MotATrouver)
+else:
+    print("mot non trouer")
+    
+
+print()
+#Trouver le debut ou la fin de chaine
+fin = "image.png".endswith(".png")
+print("le format de l'image est en PNG => ", fin)
+start = "image.png".startswith("video")
+print("la chaine 'image.png' commence par video =>", start)
+
+print()
+#Les oprateurs
+print("Les operateurs")
+    #Les operateurs mathematiques
+print("Les operateurs mathematiques")
+print("Affiche 5 + 2 =", 5 + 2)
+print("Affiche 5 - 2 =", 5 - 2)
+print("Affiche 5 * 2 =", 5 * 2)
+print("Affiche 5 / 2 =", 5 / 2)
+
+    #Sur une chaine de caractere
+python = "Python"
+print(f"Affiche le mot {python} 3 fois : ", python * 3)
+
+#Le modulo %
+rest = 7 % 4
+print("La division de 7 par 4 donne un reste de : ", rest)
+# le quotient //
+quotient_1 = 10 / 3
+print("le quotient de 10 et 3 est : ", quotient_1)
+quotient = 10 // 3
+print("le quotient de 10 et 3 est : ", quotient)
+# la puissance **
+puissance = 8**3
+print("8 a la puissance de 3 est : ", puissance)
+
+    #les operateurs d'assignation
+    
+x=5
+print("Affiche la valeur assignée a x => ", x)
+x += 1
+print("Apres l'ajout de 1 a x on obtient : ", x)
+
+    #les operateurs de comparaison
+m = 12
+n = 6
+if m > n:
+    print("m est superieur a n")
+else:
+    print("m est inferieur à n")
+    
+    
+print()
+#Le formatage des chaines de caracteres
+print("Le formatage des chaines de caracteres")
+    # La concatenation
+print("Bonjour" + "tout" + "le monde")
+    # les f-string
+nom = "Gueye"
+print(f"le nom {nom} ne figure pas dans notre base de donnée")
+    # La methode format
+mon_age = 25
+phrase = "je suis agé de {age} ans".format(age=mon_age)
+print(phrase)
+phrase_2 = "je suis agé de {0} ans, {0} n'est pas trop vieu!".format(mon_age)
+print(phrase_2)
+
+name = "Mohamed"
+age = 27
+phrase_3 = "Mon nom est {0}, je suis agé de {1} ans.".format(name, age)
+print(phrase_3)
+
+
+print()
+  
